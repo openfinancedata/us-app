c90c1b0f84ddc11805e56f30fa5fe7a3c6fe6109
openfinancedata <open.finance.data@gmail.com>
2024-06-10 11:09:26 +0000

    Fix Links

--- app-interface.js
+++ app-interface.js
@@ -2,18 +2,18 @@
 function cl(tx) { console.log(tx); }
 cl("Welcome to the Bridging.App");
 
-// Change 1st & 2nd Cols to Links in table
+// Change 3rd & 4th Cols to Links in table
 function namesToLinks(px) {
   let table, tr, i;
   table = document.getElementById(px);
   tr = table.getElementsByTagName("tr");
   for (i = 0; i < tr.length; i++) {
-    var makeLink = table.rows[i].cells[1].innerHTML;
-    var makeLink2 = table.rows[i].cells[0].innerHTML;
-    var web = makeLink.link(table.rows[i].cells[1].innerHTML);
-    var web2 = makeLink2.link(table.rows[i].cells[1].innerHTML);
-    table.rows[i].cells[0].innerHTML = web2;
-    table.rows[i].cells[1].innerHTML = web;
+    var makeLink = table.rows[i].cells[2].innerHTML;
+    var makeLink2 = table.rows[i].cells[3].innerHTML;
+    var web = makeLink.link(table.rows[i].cells[2].innerHTML);
+    var web2 = makeLink2.link(table.rows[i].cells[3].innerHTML);
+    table.rows[i].cells[3].innerHTML = web2;
+    table.rows[i].cells[2].innerHTML = web;
   }
 }
 
