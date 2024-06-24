84ce9a7d70aee69233be890aca0a51288b406171
openfinancedata <open.finance.data@gmail.com>
2024-06-24 00:26:43 +0000

    Multiple Updates

--- app-vars.js
+++ app-vars.js
@@ -1,9 +1,9 @@
 // Ctrl C + V > index.html
-const varVer = "2024-06-07";			// Version for announcment
-const BATSV = "2024-06-07 - 16:28";	// BATimeStampVer 
-const sWTSV = "240607";			// serviceWorkerTimeStampVer 
+const varVer = "2024-06-23";			// Version for announcment
+const BATSV = "2024-06-23 - 16:28";	// BATimeStampVer 
+const sWTSV = "240623";			// serviceWorkerTimeStampVer 
 const URLtoSW = "/sw" + sWTSV + ".js";
-var dLS = ukGoogleCSV; // Pick CSV Source from app-datasets.js or URL String of own source
+var dLS = OFDStatusGoogleCSV; // Pick CSV Source from app-datasets.js or URL String of own source
 // -------------------- End index section --------------------
 
 var autorun = true;
